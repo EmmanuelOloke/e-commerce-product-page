f28f58acfe5111bbdf32c6cb9f47a83ad39292fa
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-01-12 14:47:08 +0100

    switch main layout image on click

--- src/Components/LightBox/LightBox.js
+++ src/Components/LightBox/LightBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Product1 from '../../images/image-product-1.jpg';
 import Product1Thumbnail from '../../images/image-product-1-thumbnail.jpg';
-// import Product2 from '../../images/image-product-2.jpg';
+import Product2 from '../../images/image-product-2.jpg';
 import Product2Thumbnail from '../../images/image-product-2-thumbnail.jpg';
-// import Product3 from '../../images/image-product-3.jpg';
+import Product3 from '../../images/image-product-3.jpg';
 import Product3Thumbnail from '../../images/image-product-3-thumbnail.jpg';
-// import Product4 from '../../images/image-product-4.jpg';
+import Product4 from '../../images/image-product-4.jpg';
 import Product4Thumbnail from '../../images/image-product-4-thumbnail.jpg';
 import IconMinus from '../../images/icon-minus.svg';
 import IconPlus from '../../images/icon-plus.svg';
@@ -30,24 +30,31 @@ function LightBox() {
     }
 
     const price = (unit, qty) => {
-        if (qty === 0){
+        let totalPrice = "$" + unit * qty;
+        if (totalPrice === "$0.00"){
             document.getElementById('total').innerHTML = "$125.00";
         }
-        let totalPrice = "$" + unit * qty;
         document.getElementById('total').innerHTML = totalPrice + ".00";
         return totalPrice;
+    }
+
+    const switchImage = (productImage) => {
+        let imgSource = document.getElementById('mainImage').src;
+        imgSource = productImage;
+        document.getElementById('mainImage').src = imgSource;
+        return imgSource;
     }
 
     return (
         <div className="lightbox-container">
             <div className="lightbox-images">
-                <img className="lightbox-main-image" src={Product1} alt="main product"/>
+                <img id="mainImage" className="lightbox-main-image" src={Product1} alt="main product"/>
 
                 <div className="lightbox-thumbnails">
-                    <img src={Product1Thumbnail} alt="product 1 thumbnail"/>
-                    <img src={Product2Thumbnail} alt="product 2 thumbnail"/>
-                    <img src={Product3Thumbnail} alt="product 3 thumbnail"/>
-                    <img src={Product4Thumbnail} alt="product 4 thumbnail"/>
+                    <img src={Product1Thumbnail} alt="product 1 thumbnail" onClick={() => switchImage(Product1)}/>
+                    <img src={Product2Thumbnail} alt="product 2 thumbnail" onClick={() => switchImage(Product2)}/>
+                    <img src={Product3Thumbnail} alt="product 3 thumbnail" onClick={() => switchImage(Product3)}/>
+                    <img src={Product4Thumbnail} alt="product 4 thumbnail" onClick={() => switchImage(Product4)}/>
                 </div>
             </div>
 
