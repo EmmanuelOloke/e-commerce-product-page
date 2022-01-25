155cd45646bb46141ebc1164603575dc53dc4f67
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-01-25 12:58:36 +0100

    filled cart design adjustments

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -33,13 +33,13 @@ function NavBar() {
             </div>
 
             <div className="cart-items">
-                <p className="cart-content">Cart</p>
+                <p className="title bold">Cart</p>
                 <hr style={{opacity: 0.3}}/>
                 <div className="item-details cart-content">
                     <img className="thumbnail" src={Product1Thumbnail} alt="Product 1 Thumbnail"/>
                     <div className="details-text">
                         <div>Fall Limited Edition Sneakers</div>
-                        <div>$125.00 x 3 <span className="cart-total">$375.00</span></div>
+                        <div>$125.00 x 3 <span className="bold">$375.00</span></div>
                     </div>
                     <img src={Delete} alt="Delete Button"/>
                 </div>
